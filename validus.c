708474377ea5697a0ca220491ba8b1228e15e5e7
Ryan M. Lederman <lederman@gmail.com>
2023-07-05 19:09:39 -0600

    remove test code

--- validus.c
+++ validus.c
@@ -24,7 +24,6 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 #include "validus.h"
-//#include <stdlib.h>
 #include <string.h>
 
 void validus_init(validus_state* state)
