89ab891f06ad3bfc84e077eae46dc9793a3bd28f
Ryan M. Lederman <lederman@gmail.com>
2025-11-28 05:46:24 -0700

    bump version to 1.0.5, regen docs

--- validuscli.c
+++ validuscli.c
@@ -4,7 +4,7 @@
  *
  * @author    Ryan M. Lederman \<lederman@gmail.com\>
  * @date      2004-2025
- * @version   1.0.4
+ * @version   1.0.5
  * @copyright The MIT License (MIT)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
